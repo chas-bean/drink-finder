7d610a0c91fde3766eb5a268bb454036900b512c
Chas Bean <charlesparkerbean@gmail.com>
2023-06-14 10:03:42 -0400

    Add drink page fetching w/ suspense

--- src/app/drinks/[id]/page.tsx
+++ src/app/drinks/[id]/page.tsx
@@ -1,3 +1,24 @@
-export default function DrinkPage() {
-  return <div>A drink</div>;
+import showDrink from "@/lib/drinks/api/showDrink";
+import { Metadata } from "next";
+
+interface DrinkPageProps {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
+export async function generateMetadata({
+  params,
+}: DrinkPageProps): Promise<Metadata> {
+  const drink = await showDrink({ id: params.id });
+
+  return {
+    title: drink?.name,
+  };
+}
+
+export default async function DrinkPage({ params }: DrinkPageProps) {
+  const drinkId = params.id;
+  const drink = await showDrink({ id: drinkId });
+
+  return <div>{drink?.name}</div>;
 }
